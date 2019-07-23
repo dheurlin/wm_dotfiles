6cd5f5769f90a24062a31a55f0c6514cdf63d343
Daniel Heurlin <daniel.heurlin@gmail.com>
2019-07-23 16:39:12 +0200

    Fix broken audio buttons

--- config/scripts/volume.sh
+++ config/scripts/volume.sh
@@ -5,6 +5,6 @@
 volSound=/home/danielheurlin/.config/scripts/sounds/volume.ogg
 # volSound=/usr/share/sounds/gnome/default/alerts/bark.ogg
 
-pactl set-sink-mute 0 0 && \
-pactl set-sink-volume 0 $1 && \
+pactl set-sink-mute @DEFAULT_SINK@ 0 && \
+pactl set-sink-volume @DEFAULT_SINK@ $1 && \
 (pacmd list-sink-inputs | grep -c 'state: RUNNING' || play $volSound)
