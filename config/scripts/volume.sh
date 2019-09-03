9b5cda4d3a1cc6dd51cfbef33a624ff59d75ea85
Daniel Heurlin <daniel.heurlin@gmail.com>
2019-09-03 13:41:16 +0200

    Multi monitor bspwm config

--- config/scripts/volume.sh
+++ config/scripts/volume.sh
@@ -2,9 +2,14 @@
 
 # Argument should be e.g. +5% or -5%
 
-volSound=/home/danielheurlin/.config/scripts/sounds/volume.ogg
-# volSound=/usr/share/sounds/gnome/default/alerts/bark.ogg
+# volSound=/home/danielheurlin/.config/scripts/sounds/bark.ogg
+volSound=/home/danielheurlin/.config/scripts/sounds/bark.wav
+play_sound() { aplay $volSound; }
 
-pactl set-sink-mute @DEFAULT_SINK@ 0 && \
-pactl set-sink-volume @DEFAULT_SINK@ $1 && \
-(pacmd list-sink-inputs | grep -c 'state: RUNNING' || play $volSound)
+pactl set-sink-mute @DEFAULT_SINK@ 0
+pactl set-sink-volume @DEFAULT_SINK@ $1
+# $(pacmd list-sink-inputs | grep -c 'state: RUNNING') || play_sound
+
+if ! pacmd list-sink-inputs | grep -c 'state: RUNNING'; then
+    play_sound
+fi
