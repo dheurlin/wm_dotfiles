b8b5421a97f0f7f256401a1cafe76cfa87e01291
Daniel Heurlin <daniel.heurlin@gmail.com>
2020-07-07 20:18:29 +0200

    Include special workspaces in next / curent ws bindings

--- .xmonad/include/Navigation.hs
+++ .xmonad/include/Navigation.hs
@@ -7,24 +7,31 @@ import           Data.List
 import           Data.Char
 import           Data.Maybe
 
--- | Focuses the previous numeric workspace
+-- | Focuses the previous special or nonempty numeric workspace
 prevWS :: WindowSet -> WindowSet
 prevWS = relWS (-1)
 
--- | Focuses the next numeric workspace
+-- | Focuses the next special or nonempty numeric workspace
 nextWS :: WindowSet -> WindowSet
 nextWS = relWS 1
 
--- | Focuses a numeric workspace with an index relative to the current one
+-- | Focuses a special or numeric nonempty workspace
+--  with an index relative to the current one
 relWS :: Int -> WindowSet -> WindowSet
 relWS rel ws = SS.view newIx ws
  where
-  curWS = SS.currentTag ws
-  allWS =
+  isNonEmpty  = isJust . SS.stack
+  isCurrent w = SS.tag w == curWS
+  allNumeric =
     sortOn (read :: String -> Int)
       . filter (all isNumber)
       . map SS.tag
+      . filter (\ws -> isCurrent ws || isNonEmpty ws)
       $ SS.workspaces ws
+  allSpecial =
+    sort $ filter (not . any isNumber) . map SS.tag $ SS.workspaces ws
+  allWS  = allNumeric <> allSpecial
+  curWS  = SS.currentTag ws
   ix     = fromJust $ curWS `elemIndex` allWS
   bounds = min (length allWS - 1) . max 0
   newIx  = allWS !! bounds (ix + rel)
