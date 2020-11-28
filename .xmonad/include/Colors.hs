d2ba8636c2982c8f4deb13dbb4e5e5429828594f
Daniel Heurlin <daniel.heurlin@gmail.com>
2020-11-29 00:26:52 +0100

    XMonad: change bar accent color

--- .xmonad/include/Colors.hs
+++ .xmonad/include/Colors.hs
@@ -1,6 +1,7 @@
 module Colors where
 
-accentBg        = "#5f5287"
+-- accentBg        = "#5f5287"
+accentBg        = "#555555"
 bg              = "#1f1f1f"
 unFocusedBorder = "#000000"
-focusedBorder   = accentBg
+focusedBorder   = "#5f5287"
