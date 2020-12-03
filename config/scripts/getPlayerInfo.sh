7195f4378e76b2874be99e78824747f149b6d878
Daniel Heurlin <daniel.heurlin@gmail.com>
2020-12-04 00:03:37 +0100

    XMobar: playerctl info

---
+++ config/scripts/getPlayerInfo.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+maxlen=$1
+
+trim() {
+  if [ "$(echo "$1" | wc -m)" -ge "$maxlen" ]; then
+    echo "$(echo "$1" | cut -c -${maxlen})..."
+  else
+    echo "$1"
+  fi
+}
+
+status=$(playerctl status)
+case $status in
+  "Playing")
+    artist=$(playerctl metadata --format "{{artist}}")
+    title=$(playerctl metadata --format "{{title}}")
+    echo "$(trim "$artist"): $(trim "$title")"
+    ;;
+  *)
+    echo "-"
+    ;;
+esac
